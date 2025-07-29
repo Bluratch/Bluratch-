5a5f63d617e038d0f82be3c20722f51ca9e7fba0
Bluratch <140326873+Bluratch@users.noreply.github.com>
2025-07-29 22:51:31 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~Mm8llYbZ#A_FcKBcv1BcuTqg2IQOgOXhox7vUEnqLD0aI5BjYwtM ",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "2348110818661", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
